59e9d72f55fb537e578b21a0838b975d5a920f65
Fonkeyy <cedrick.guevel@gmail.com>
2023-10-20 08:54:58 +0200

    modify help modal

--- src/Components/InfoDialog.jsx
+++ src/Components/InfoDialog.jsx
@@ -2,26 +2,24 @@ import { useState } from 'react';
 import '../css/InfoDialog.css';
 
 const InfoDialog = () => {
-    const [isOpen, setIsOpen] = useState(false);
-
-    const openDialog = () => {
-        setIsOpen(true);
-    };
-
-    const closeDialog = () => {
-        setIsOpen(false);
-    };
+    const [isOpen, setIsOpen] = useState(true);
 
     return (
         <>
-            {!isOpen && (
-                <button id="info-dialog-btn" aria-label="infos how to play" onClick={openDialog}></button>
-            )}
-
+            <button
+                id="info-dialog-btn"
+                aria-label="rules of the game"
+                onClick={() => setIsOpen(!isOpen)}></button>
             {isOpen && (
                 <dialog id="info-dialog" open>
-                    <p>Try to click on each cards no more than once, the faster you can...</p>
-                    <button id="close-svg" onClick={closeDialog}>
+                    <p>
+                        Welcome to Memory Counter Skins, the ultimate test of your memory and speed!<br></br>
+                        In this game, your mission is to click on a unique skin with lightning speed, but
+                        beware, a single repeat click and it&lsquo;s game over! <br></br>Are you up for the
+                        challenge? Good luck!
+                    </p>
+
+                    <button id="close-dialog" onClick={() => setIsOpen(!isOpen)}>
                         Let&lsquo;s go
                     </button>
                 </dialog>
